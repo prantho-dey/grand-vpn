cd282ad5a87364b20d30d0ac0fe63ade9c89dd63
Prantho <pranthodey89@gmail.com>
2021-07-11 17:20:48 +0600

    second Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 (function ($) {
 
     'use strict';
+    
+   // jQuery CountDown
+   if( $(".countdown-clock").length ){
+    $('.countdown-clock').countdown('2021/12/20', function(event) {
+        $('.clock-days').html(event.strftime('%D:'));
+        $('.clock-hours').html(event.strftime('%H: '));
+        $('.clock-minutes').html(event.strftime('%M:'));
+        $('.clock-seconds').html(event.strftime('%S'));
+    });
+}
 
+    
 
 })(jQuery);
